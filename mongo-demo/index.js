e04fe54ba5fd482bffbb2905b561d2f6a1987c43
Subhranil Sarkar <subhranil2605@gmail.com>
2023-11-24 16:39:13 +0530

    Add schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.log('Could not connect to the MongoDB...', err.message))
+
+// Create a schema for the courses
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
