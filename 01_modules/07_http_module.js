30667128e77d4f0b0482acb65e5563ddecc7e181
Subhranil Sarkar <subhranil2605@gmail.com>
2024-03-19 23:53:03 +0530

    Add server with multiple routes handling

--- 01_modules/07_http_module.js
+++ 01_modules/07_http_module.js
@@ -3,7 +3,20 @@ const http = require('http');
 const port = 3000;
 
 // Create a server
-const server = http.createServer();
+// const server = http.createServer();
+
+// Create a server with different routes handling
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello world');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+})
 
 
 // Raise an event when a new connection is made
